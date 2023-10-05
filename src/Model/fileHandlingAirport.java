53fa2e05109cd13dde12bb4ddca39c1364a98d52
Atharva Deore <140725599+adeore09@users.noreply.github.com>
2023-10-05 23:23:24 +0530

    Delete src directory

--- src/Model/fileHandlingAirport.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-import java.util.ArrayList;
-
-abstract class fileHandlingAirport {
-    abstract public ArrayList<Airport> readJsonFile(String file_path);
-
-}
--- src/Model/fileHandlingFlight.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-import java.util.ArrayList;
-
-abstract class fileHandlingFlight {
-    abstract public ArrayList<CommercialFlight> readJsonFile(String file_path);
-    abstract public void writeJsonFile(ArrayList<CommercialFlight> flights);
-
-}
